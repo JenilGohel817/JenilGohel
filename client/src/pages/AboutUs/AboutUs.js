a448b846c39e5ff2108b793487d2387d0fdb189f
JenilGohel817 <jenilgohel817@gmail.com>
2023-09-28 01:04:14 +0530

    new

---
+++ client/src/pages/AboutUs/AboutUs.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Layout from "../../layouts/Layout";
+
+const AboutUs = () => {
+  return (
+    <>
+      <Layout>
+        <section className="jg-section">
+          <div className="jg-container">
+            <div className="jg-about-wrapper">Jenil</div>
+          </div>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export default AboutUs;
