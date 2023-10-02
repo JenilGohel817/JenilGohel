f5e88b19631433a761b319852cdc259393b7f522
JenilGohel817 <jenilgohel817@gmail.com>
2023-10-03 00:57:27 +0530

    node

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
 const PORT = process.env.PORT || 8080;
 const app = express();
 
@@ -7,6 +10,16 @@ dotenv.config({
   path: "./private/.env",
 });
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+app.use(morgan("dev"));
+
 app.listen(PORT, () => {
   console.log(`Server Started !`);
 });
+
+export default app;
