3810d00b6a73e7e9f721d4b610cff066d5ecd4e4
JenilGohel817 <jenilgohel817@gmail.com>
2023-10-03 00:19:49 +0530

    AboutUs

--- client/src/pages/AboutUs/AboutUs.js
+++ client/src/pages/AboutUs/AboutUs.js
@@ -79,130 +79,163 @@ const AboutUs = () => {
                   </h2>
                   <div className="jg-brief-info">
                     <p className="jg-brief-info-para">
-                      I'm a passionate and highly skilled software developer
-                      with a deep love for all things code. My journey in the
-                      world of software development began with a fascination for
-                      turning lines of logic into powerful, user-friendly
-                      applications. Over the years, I've honed my skills and
-                      gained invaluable experience in crafting robust,
-                      efficient, and innovative software solutions.
+                      With over a year worth of expertise provide amazing
+                      customer experiences, professional and creative Software
+                      developers
                     </p>
                     <p className="jg-brief-info-para jg-brief-info-para-last">
-                      I'm a passionate and highly skilled software developer
-                      with a deep love for all things code. My journey in the
-                      world of software development began with a fascination for
-                      turning lines of logic into powerful, user-friendly
-                      applications. Over the years, I've honed my skills and
-                      gained invaluable experience in crafting robust,
-                      efficient, and innovative software solutions.
+                      with a passion Ability to work on technologies like HTML5,
+                      CSS3, JavaScript, jQuery, AJAX, Jinja2 and knowledgeable
+                      in the Python language, the Django framework, and the
+                      React library. Knowledge of Relational Database Systems
+                      such MySQL, PostgreSQL
                     </p>
                   </div>
                   <h2 className="jg-about-heading">EXPERIENCE</h2>
                   <div className="jg-experience">
                     <div className="jg-experience-wrap">
-                      <div className="jg-experience-year">2007 - 2017</div>
+                      <div className="jg-experience-year">
+                        FEB 2023 - PRESENT
+                      </div>
                       <div className="jg-experience-designation">
-                        Framer Designer & Developer
+                        Full Stack Developer
                       </div>
-                      <div className="jg-experience-company">
-                        Bluebase Designs
-                      </div>
+                      <div className="jg-experience-company">ZealousWeb</div>
                       <p className="jg-experience-info">
-                        Sit amet luctussd fav venenatis, lectus magna fringilla
-                        inis urna, porttitor rhoncus dolor purus non enim
-                        praesent in elementum sahas facilisis leo, vel fringilla
-                        est ullamcorper eget nulla facilisi etisam dignissim
-                        diam quis enim lobortis viverra orci sagittis eu
-                        volutpat odio facilisis mauris sit.
+                        Front-End: React, Redux, HTML5, CSS3, JavaScript (ES6+)
+                        Back-End: Node.js, Express.js Database: MongoDB,
+                        Mongoose RESTful API Development Responsive Web Design
+                        Version Control: Git, GitHub/GitLab Skills: Redux ·
+                        Node.js · React.js · JavaScript
                       </p>
                     </div>
                     <div className="jg-experience-wrap">
-                      <div className="jg-experience-year">2007 - 2017</div>
+                      <div className="jg-experience-year">
+                        JUL 2022 - OCT 2022
+                      </div>
                       <div className="jg-experience-designation">
-                        Framer Designer & Developer
+                        Full Stack Developer Grade 3
                       </div>
                       <div className="jg-experience-company">
-                        Bluebase Designs
+                        Evol Technobits Digital Pvt. Ltd.
                       </div>
                       <p className="jg-experience-info">
-                        Sit amet luctussd fav venenatis, lectus magna fringilla
-                        inis urna, porttitor rhoncus dolor purus non enim
-                        praesent in elementum sahas facilisis leo, vel fringilla
-                        est ullamcorper eget nulla facilisi etisam dignissim
-                        diam quis enim lobortis viverra orci sagittis eu
-                        volutpat odio facilisis mauris sit.
+                        Skills: C++ · Data Structures · Algorithms · C
+                        (Programming Language)
+                      </p>
+                    </div>
+                    <div className="jg-experience-wrap">
+                      <div className="jg-experience-year">
+                        JUL 2022 - OCT 2022
+                      </div>
+                      <div className="jg-experience-designation">
+                        Full Stack Developer Grade 2
+                      </div>
+                      <div className="jg-experience-company">
+                        Evol Technobits Digital Pvt. Ltd.
+                      </div>
+                      <p className="jg-experience-info">
+                        Skills: Team Leadership · Team Building · Team
+                        Management
+                      </p>
+                    </div>
+                    <div className="jg-experience-wrap">
+                      <div className="jg-experience-year">
+                        FEB 2022 - JUL 2022
+                      </div>
+                      <div className="jg-experience-designation">
+                        Full Stack Developer Grade 1
+                      </div>
+                      <div className="jg-experience-company">
+                        Evol Technobits Digital Pvt. Ltd.
+                      </div>
+                      <p className="jg-experience-info">
+                        Skills: Front-End Development · React.js · Web
+                        Development · Web Design · Responsive Web Design ·
+                        JavaScript · Django · jQuery
+                      </p>
+                    </div>
+                    <div className="jg-experience-wrap  jg-experience-wrap-last">
+                      <div className="jg-experience-year">
+                        DEC 2021 - FEB 2022
+                      </div>
+                      <div className="jg-experience-designation">
+                        Full Stack Developer Internship
+                      </div>
+                      <div className="jg-experience-company">
+                        Evol Technobits Digital Pvt. Ltd.
+                      </div>
+                      <p className="jg-experience-info">
+                        Skills: Search Engine Optimization (SEO) · System
+                        Testing · Figma · Adobe Photoshop · Web Design ·
+                        Responsive Web Design · Problem Solving
+                      </p>
+                    </div>
+                  </div>
+                  <h2 className="jg-about-heading">AWARDS</h2>
+                  <div className="jg-experience">
+                    <div className="jg-experience-wrap">
+                      <div className="jg-experience-year">
+                        DEC 2021 - OCT 2022
+                      </div>
+                      <div className="jg-experience-designation">
+                        Appreciation Letter
+                      </div>
+                      <div className="jg-experience-company">
+                        Evol Technobits Digital Pvt. Ltd.
+                      </div>
+                      <p className="jg-experience-info">
+                        Jenil Gohel has shown their quality of work in a recent
+                        project for Evol Technobits Digital Pvt. Ltd
                       </p>
                     </div>
                     <div className="jg-experience-wrap jg-experience-wrap-last">
-                      <div className="jg-experience-year">2007 - 2017</div>
+                      <div className="jg-experience-year">
+                        DEC 2021 - OCT 2022
+                      </div>
                       <div className="jg-experience-designation">
-                        Framer Designer & Developer
+                        Increment Letter
                       </div>
                       <div className="jg-experience-company">
-                        Bluebase Designs
+                        Evol Technobits Digital Pvt. Ltd.
                       </div>
                       <p className="jg-experience-info">
-                        Sit amet luctussd fav venenatis, lectus magna fringilla
-                        inis urna, porttitor rhoncus dolor purus non enim
-                        praesent in elementum sahas facilisis leo, vel fringilla
-                        est ullamcorper eget nulla facilisi etisam dignissim
-                        diam quis enim lobortis viverra orci sagittis eu
-                        volutpat odio facilisis mauris sit.
+                        In Six Month I have 2 increment letter for my quality of
+                        work
                       </p>
                     </div>
                   </div>
                   <h2 className="jg-about-heading">EDUCATION</h2>
                   <div className="jg-experience">
                     <div className="jg-experience-wrap">
-                      <div className="jg-experience-year">2007 - 2017</div>
+                      <div className="jg-experience-year">
+                        JAN 2015 - DEC 2016
+                      </div>
                       <div className="jg-experience-designation">
-                        Framer Designer & Developer
+                        Shree P.V Doshi Vidyalay
                       </div>
-                      <div className="jg-experience-company">
-                        Bluebase Designs
-                      </div>
-                      <p className="jg-experience-info">
-                        Sit amet luctussd fav venenatis, lectus magna fringilla
-                        inis urna, porttitor rhoncus dolor purus non enim
-                        praesent in elementum sahas facilisis leo, vel fringilla
-                        est ullamcorper eget nulla facilisi etisam dignissim
-                        diam quis enim lobortis viverra orci sagittis eu
-                        volutpat odio facilisis mauris sit.
-                      </p>
+                      <div className="jg-experience-company">SSC</div>
+                      <p className="jg-experience-info">Percentage - 62.27%</p>
                     </div>
                     <div className="jg-experience-wrap">
-                      <div className="jg-experience-year">2007 - 2017</div>
+                      <div className="jg-experience-year">
+                        JAN 2017 - DEC 2018
+                      </div>
                       <div className="jg-experience-designation">
-                        Framer Designer & Developer
+                        Shree P.V Doshi Vidyalay
                       </div>
-                      <div className="jg-experience-company">
-                        Bluebase Designs
-                      </div>
-                      <p className="jg-experience-info">
-                        Sit amet luctussd fav venenatis, lectus magna fringilla
-                        inis urna, porttitor rhoncus dolor purus non enim
-                        praesent in elementum sahas facilisis leo, vel fringilla
-                        est ullamcorper eget nulla facilisi etisam dignissim
-                        diam quis enim lobortis viverra orci sagittis eu
-                        volutpat odio facilisis mauris sit.
-                      </p>
+                      <div className="jg-experience-company">HSC</div>
+                      <p className="jg-experience-info">Percentage - 50.54%</p>
                     </div>
                     <div className="jg-experience-wrap jg-experience-wrap-last">
-                      <div className="jg-experience-year">2007 - 2017</div>
+                      <div className="jg-experience-year">
+                        DEC 2018 - JAN 2021
+                      </div>
                       <div className="jg-experience-designation">
-                        Framer Designer & Developer
+                        Adarsh Educational Campus
                       </div>
-                      <div className="jg-experience-company">
-                        Bluebase Designs
-                      </div>
-                      <p className="jg-experience-info">
-                        Sit amet luctussd fav venenatis, lectus magna fringilla
-                        inis urna, porttitor rhoncus dolor purus non enim
-                        praesent in elementum sahas facilisis leo, vel fringilla
-                        est ullamcorper eget nulla facilisi etisam dignissim
-                        diam quis enim lobortis viverra orci sagittis eu
-                        volutpat odio facilisis mauris sit.
-                      </p>
+                      <div className="jg-experience-company">BCA</div>
+                      <p className="jg-experience-info">Percentage - 61%</p>
                     </div>
                   </div>
                   <h2 className="jg-about-heading">SKILLS</h2>
@@ -213,29 +246,47 @@ const AboutUs = () => {
                       <p>Non enim praesent</p>
                     </div>
                     <div className="jg-skills">
-                      <span className="jg-skills-percent">85%</span>
+                      <span className="jg-skills-percent">50%</span>
                       <h3 className="jg-skills-name">Python</h3>
                       <p>Non enim praesent</p>
                     </div>
                     <div className="jg-skills">
                       <span className="jg-skills-percent">85%</span>
-                      <h3 className="jg-skills-name">Figma</h3>
+                      <h3 className="jg-skills-name">ReactJs</h3>
                       <p>Non enim praesent</p>
                     </div>
                     <div className="jg-skills">
                       <span className="jg-skills-percent">85%</span>
-                      <h3 className="jg-skills-name">React</h3>
+                      <h3 className="jg-skills-name">NodeJs</h3>
                       <p>Non enim praesent</p>
                     </div>
                     <div className="jg-skills">
                       <span className="jg-skills-percent">85%</span>
-                      <h3 className="jg-skills-name">Node</h3>
+                      <h3 className="jg-skills-name">MySql</h3>
                       <p>Non enim praesent</p>
                     </div>
                     <div className="jg-skills">
                       <span className="jg-skills-percent">85%</span>
-                      <h3 className="jg-skills-name">Adobe XD</h3>
+                      <h3 className="jg-skills-name">MongoDB</h3>
                       <p>Non enim praesent</p>
+                    </div>
+                  </div>
+                  <h2 className="jg-about-heading">PROJECTS</h2>
+                  <div className="jg-experience">
+                    <div className="jg-experience-wrap jg-experience-wrap-last">
+                      <div className="jg-experience-year">
+                        DEC 2021 - OCT 2022
+                      </div>
+                      <div className="jg-experience-designation">
+                        complete 25+ projects
+                      </div>
+                      <div className="jg-experience-company">
+                        ReactJs, NodeJs, Django, Python, MySql, MongoDB
+                      </div>
+                      <p className="jg-experience-info">
+                        portfolio site, E-commerce site, Python projects, Django
+                        Web Projects
+                      </p>
                     </div>
                   </div>
                 </div>
