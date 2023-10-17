01889292f3ba7a379427f46da7ca3459ea5c6fc1
JenilGohel817 <jenilgohel817@gmail.com>
2023-10-18 00:56:22 +0530

    remove upload

--- server/middleware/fileUpload.js
+++ server/middleware/fileUpload.js
@@ -2,9 +2,9 @@ import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
-  },
+  // destination: function (req, file, cb) {
+  //   cb(null, "./uploads");
+  // },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
