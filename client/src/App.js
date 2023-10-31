9ca7cea7560709a8765b5b8c3695f0a90d8db6ad
JenilGohel817 <jenilgohel817@gmail.com>
2023-11-01 02:02:51 +0530

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Contact from "./components/Contact/Contact.js";
 import Project from "./pages/Project/Project.js";
 import AboutUs from "./pages/AboutUs/AboutUs.js";
 import AdminHome from "./admin/adminHome/AdminHome.js";
-import LoginFrom from "./components/Login/LoginFrom.js";
 import AdminUpdate from "./admin/adminUpdate/adminUpdate.js";
 import RegisterFrom from "./components/Register/RegisterFrom.js";
 
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route path={""} element={<Home />}></Route>
         <Route path={"/Admin"} element={<AdminHome />}></Route>
-        <Route path={"/Login"} element={<LoginFrom />}></Route>
         <Route path={"/Register"} element={<RegisterFrom />}></Route>
         <Route path={"/Admin/:slug"} element={<AdminUpdate />}></Route>
         <Route path={"/ContactUs"} element={<Contact />}></Route>
