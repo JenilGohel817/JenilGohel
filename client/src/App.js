f7000f12ad069fdf2468136b70322de5363a4083
JenilGohel817 <jenilgohel817@gmail.com>
2023-11-02 00:32:20 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import Project from "./pages/Project/Project.js";
 import AboutUs from "./pages/AboutUs/AboutUs.js";
 import AdminHome from "./admin/adminHome/AdminHome.js";
 import AdminUpdate from "./admin/adminUpdate/adminUpdate.js";
-import RegisterFrom from "./components/Register/RegisterFrom.js";
 import LoginFrom from "./components/Login/LoginFrom.js";
+import RegisterFrom from "./components/Register/RegisterFrom.js";
 
 function App() {
   return (
