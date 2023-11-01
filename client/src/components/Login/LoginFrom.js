be7db85a87a336efa6b707fe499ad01903ce913e
JenilGohel817 <jenilgohel817@gmail.com>
2023-11-02 00:48:28 +0530

    Login And Register

---
+++ client/src/components/Login/LoginFrom.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Layout from "../../layouts/Layout.js";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import "./LoginFrom.css";
+
+const LoginFrom = () => {
+  const [loading, setLoading] = useState(false);
+  const [LfEmail, setLfEmail] = useState("");
+  const [Lfpassword, setLfPassword] = useState("");
+  const navigate = useNavigate();
+
+  const LoginSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+
+      const loginValue = {
+        Email: LfEmail,
+        Password: Lfpassword,
+      };
+
+      console.log(loginValue);
+
+      const data = await axios.post(
+        `${process.env.REACT_APP_JGAPI_V1}/auth/login`,
+        loginValue
+      );
+
+      if (data?.success) {
+        setLoading(false);
+        toast.success(data.message);
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <>
+      <Layout>
+        <section className="jg-section">
+          <div className="jg-container">
+            <div className="jg-register-grid">
+              <div className="jg-register-item">
+                <div className="jg-heading-wrap">
+                  <div className="jg-heading-subtitle">Login</div>
+                  <h2>Help us get to know you:</h2>
+                </div>
+                <form
+                  className="jg-contact-form"
+                  method="post"
+                  onSubmit={LoginSubmit}
+                  encType="multipart/form-data"
+                >
+                  <div className="form-grid-item">
+                    <label htmlFor="Email" className="input-label">
+                      Email
+                    </label>
+                    <input
+                      className="form-input"
+                      name="Email"
+                      type="email"
+                      onChange={(e) => {
+                        setLfEmail(e.target.value);
+                      }}
+                      placeholder="Email"
+                      required
+                    />
+                  </div>
+                  <div className="form-grid-item jg-pt30">
+                    <label htmlFor="Password" className="input-label">
+                      Password
+                    </label>
+                    <input
+                      className="form-input"
+                      name="Password"
+                      onChange={(e) => {
+                        setLfPassword(e.target.value);
+                      }}
+                      type="password"
+                      placeholder="Password"
+                      required
+                    />
+                  </div>
+                  <div className="jg-pt30 jg-text-right">
+                    <button type="submit" className="jg-button">
+                      {loading ? "Login..." : "Login"}
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export default LoginFrom;
---
+++ client/src/components/Register/RegisterFrom.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import "./RegisterFrom.css";
+import axios from "axios";
+import Layout from "../../layouts/Layout.js";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+const RegisterFrom = () => {
+  const [loading, setLoading] = useState(false);
+  const [Rfpassword, setRfPassword] = useState("");
+  const [RfEmail, setRfEmail] = useState("");
+  const navigate = useNavigate();
+
+  const RegisterSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    try {
+      const registerValue = {
+        Email: RfEmail,
+        Password: Rfpassword,
+      };
+
+      const data = await axios.post(
+        `${process.env.REACT_APP_JGAPI_V1}/auth/register`,
+        registerValue
+      );
+
+      if (data?.data?.success) {
+        toast.success(data.data.message);
+        setLoading(false);
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <>
+      <Layout>
+        <section className="jg-section">
+          <div className="jg-container">
+            <div className="jg-register-grid">
+              <div className="jg-register-item">
+                <div className="jg-heading-wrap">
+                  <div className="jg-heading-subtitle">Register</div>
+                  <h2>Help us get to know you:</h2>
+                </div>
+                <form
+                  className="jg-contact-form"
+                  method="post"
+                  onSubmit={RegisterSubmit}
+                  encType="multipart/form-data"
+                >
+                  <div className="form-grid-item">
+                    <label htmlFor="Email" className="input-label">
+                      Email
+                    </label>
+                    <input
+                      className="form-input"
+                      name="Email"
+                      type="email"
+                      onChange={(e) => {
+                        setRfEmail(e.target.value);
+                      }}
+                      placeholder="Email"
+                      required
+                    />
+                  </div>
+                  <div className="form-grid-item jg-pt30">
+                    <label htmlFor="Password" className="input-label">
+                      Password
+                    </label>
+                    <input
+                      className="form-input"
+                      name="Password"
+                      onChange={(e) => {
+                        setRfPassword(e.target.value);
+                      }}
+                      type="password"
+                      placeholder="Password"
+                      required
+                    />
+                  </div>
+                  <div className="form-grid-item jg-pt30">
+                    <label htmlFor="forgetPassword" className="input-label">
+                      Forget Password
+                    </label>
+                  </div>
+                  <div className="jg-pt30 jg-text-right">
+                    <button type="submit" className="jg-button">
+                      {loading ? "Register..." : "Register"}
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export default RegisterFrom;
