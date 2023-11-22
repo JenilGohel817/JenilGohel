550777f8dfbbef78a076e071d6ac72eaca6ffea2
JenilGohel817 <jenilgohel817@gmail.com>
2023-11-22 23:32:52 +0530

    open

--- client/src/layouts/Layout.js
+++ client/src/layouts/Layout.js
@@ -4,6 +4,17 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PreLoader from "../components/PreLoader/PreLoader.js";
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+
+<Helmet>
+  <meta property="og:title" content="Jenil Gohel" />
+  <meta property="og:description" content="Jenil Gohel" />
+  <meta
+    property="og:image"
+    content="https://res.cloudinary.com/dccsz6yct/image/upload/v1700675491/negcohs5v5j5lhifrngp.png"
+  />
+  <meta property="og:url" content="https://jenil-gohel.vercel.app" />
+</Helmet>;
 
 const Layout = ({ children }) => {
   const [loading, Setloading] = useState(false);
