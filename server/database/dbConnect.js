309a8a5e55b92df2be7a844f2d8018e85088c58b
JenilGohel817 <jenilgohel817@gmail.com>
2023-11-29 23:38:21 +0530

    db error

--- server/database/dbConnect.js
+++ server/database/dbConnect.js
@@ -5,38 +5,33 @@ dotenv.config({
   path: "./private/.env",
 });
 
-let dbConnect;
+const dbConnect = mysql2.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-function handleDisconnect() {
-  dbConnect = mysql2.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  });
+dbConnect.connect(function (err) {
+  if (err) {
+    console.log(`connectionRequest Failed ${err.stack}`);
+  } else {
+    console.log(`DB connectionRequest Successful ${dbConnect.threadId}`);
+  }
+});
 
-  dbConnect.connect(function (err) {
-    if (err) {
-      console.log(`connectionRequest Failed ${err.stack}`);
-    } else {
-      console.log(`DB connectionRequest Successful ${dbConnect.threadId}`);
-    }
-  });
-
-  dbConnect.on("error", (err) => {
-    console.error("Database error:", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
+dbConnect.on("error", (err) => {
+  console.error("Database error:", err);
+  if (err.code === "PROTOCOL_CONNECTION_LOST") {
+    console.log("Reconnecting to the database...");
+    dbConnect.connect();
+  } else {
+    throw err;
+  }
+});
 
 export default dbConnect;
