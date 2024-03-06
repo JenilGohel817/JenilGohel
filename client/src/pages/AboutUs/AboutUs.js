6a756a7b216baf8d0b988f66fc8e8e5c7ebb3b0d
JenilGohel817 <jenilgohel817@gmail.com>
2024-03-07 01:09:54 +0530

    Content Update

--- client/src/pages/AboutUs/AboutUs.js
+++ client/src/pages/AboutUs/AboutUs.js
@@ -96,16 +96,15 @@ const AboutUs = () => {
                   </h2>
                   <div className="jg-brief-info">
                     <p className="jg-brief-info-para">
-                      Professional and creative Software developers with more
-                      than one and a half years of experience in providing
-                      exceptional client experiences
+                      Dedicated and skilled Full Stack Developer with 2 years of
+                      hands-on experience in designing, developing, and
+                      maintaining web applications.
                     </p>
                     <p className="jg-brief-info-para jg-brief-info-para-last">
-                      The ability to work with technologies including HTML5,
-                      CSS3, JavaScript, jQuery, AJAX, Redux, and Jinja2 as well
-                      as knowledge of the Python and JavaScript programming
-                      languages. being familiar with relational database systems
-                      like MySQL, PostgreSQL, and mongodb
+                      Proficient in both front-end and back-end technologies,
+                      with a strong focus on delivering high-quality, scalable
+                      solutions. Adept at collaborating with cross-functional
+                      teams and staying updated on the latest industry trends
                     </p>
                   </div>
                   <h2 className="jg-about-heading">EXPERIENCE</h2>
@@ -115,30 +114,60 @@ const AboutUs = () => {
                         FEB 2023 - PRESENT
                       </div>
                       <div className="jg-experience-designation">
-                        Full Stack Developer
+                        Associate Web Developer
                       </div>
-                      <div className="jg-experience-company">ZealousWeb</div>
+                      <div className="jg-experience-company">
+                        Zealousweb Technologies Pvt. Ltd, Ahmedabad
+                      </div>
                       <div className="jg-experience-info-list">
                         <ul>
                           <li>
-                            Front-End: React, Redux, HTML5, CSS3, JavaScript
+                            Front-End : React, Redux, HTML5, CSS3, JavaScript
                             (ES6+)
                           </li>
+                          <li>Back-End : Node.js, Express.js</li>
+                          <li>Database : MySQL, MongoDB</li>
+                          <li>RESTful API Development</li>
+                          <li>Responsive Web Design</li>
+                          <li>Version Control : Git, GitHub/GitLab</li>
                           <li>
-                            Back-End: Node.js, Express.js Database: MongoDB,
-                            Mongoose
+                            Collaborated with cross-functional teams to gather
+                            and analyse project requirements, ensuring alignment
+                            with business goals.
                           </li>
-                          <li>RESTful API Development Responsive Web</li>
-                          <li>Design Version Control: Git, GitHub/GitLab</li>
                           <li>
-                            Skills: Redux · Node.js · React.js · JavaScript
+                            Developed and maintained scalable and responsive web
+                            applications using a variety of technologies such as
+                            HTML, CSS, JavaScript, React.js, Node.js, and
+                            Express.js.
+                          </li>
+                          <li>
+                            Implemented RESTful APIs and integrated them into
+                            the front-end to enhance application functionality
+                          </li>
+                          <li>
+                            Conducted thorough testing and debugging to identify
+                            and resolve software defects, ensuring optimal
+                            performance and user experience.
+                          </li>
+                          <li>
+                            Utilised version control systems, such as Git, for
+                            efficient collaboration and code management.
+                          </li>
+                          <li>
+                            Collaborated with UX/UI designers to implement
+                            visually appealing and user-friendly interfaces.
+                          </li>
+                          <li>
+                            Integrated third-party APIs to enhance application
+                            features and functionality.
                           </li>
                         </ul>
                       </div>
                     </div>
                     <div className="jg-experience-wrap">
                       <div className="jg-experience-year">
-                        JUL 2022 - OCT 2022
+                        OCT 2022 - FEB 2023
                       </div>
                       <div className="jg-experience-designation">
                         Full Stack Developer Grade 3
@@ -148,11 +177,27 @@ const AboutUs = () => {
                       </div>
                       <div className="jg-experience-info-list">
                         <ul>
-                          <li>ReactJs</li>
-                          <li>NodeJs</li>
-                          <li>C++</li>
-                          <li>Data Structures</li>
-                          <li>Algorithms</li>
+                          <li>
+                            Assisted in the development of web applications,
+                            focusing on both front-end and back-end components.
+                          </li>
+                          <li>
+                            Collaborated with senior developers to understand
+                            and implement technical requirements.
+                          </li>
+                          <li>
+                            Conducted unit testing and assisted in the
+                            identification and resolution of software defects.
+                          </li>
+                          <li>
+                            Worked closely with the QA team to ensure the
+                            quality and reliability of the software.
+                          </li>
+                          <li>
+                            Participated in code reviews to ensure code quality,
+                            adherence to coding standards, and knowledge sharing
+                            within the team.
+                          </li>
                         </ul>
                       </div>
                     </div>
@@ -168,9 +213,28 @@ const AboutUs = () => {
                       </div>
                       <div className="jg-experience-info-list">
                         <ul>
-                          <li>Team Leadership</li>
-                          <li>Team Building</li>
-                          <li>Team Management</li>
+                          <li>
+                            Continually identified and delivered scalable
+                            solutions.
+                          </li>
+                          <li>
+                            Mentored new junior front-end developer on team in
+                            expanding JavaScript, HTML,CSS, UI/UX and Figma
+                            Skillset.
+                          </li>
+                          <li>
+                            Led troubleshooting, Fixed bugs and other issues,
+                            and updated sites throughout the production
+                            lifecycle.Maintain websites, including content
+                            updates, and regular upgrades to ensure system
+                            usability.
+                          </li>
+                          <li>
+                            Collaborated with web designers, back-end developers
+                            to design, build, test and improve web Products.
+                            Worked to assess competing websites in regards to
+                            content, look, and feel.
+                          </li>
                         </ul>
                       </div>
                     </div>
@@ -186,13 +250,22 @@ const AboutUs = () => {
                       </div>
                       <div className="jg-experience-info-list">
                         <ul>
-                          <li>Front-End Development</li>
-                          <li>React.js</li>
-                          <li>Web Development</li>
-                          <li>Web Design</li>
-                          <li>JavaScript</li>
-                          <li>Django</li>
-                          <li>jQuery</li>
+                          <li>
+                            The capability to operate with backend Node.js and
+                            frontend React.js technologies.
+                          </li>
+                          <li>
+                            Knowledge of Relational Database System such MySQL,
+                            PostgreSQL.
+                          </li>
+                          <li>
+                            Good knowledge of data structures and algorithms.
+                          </li>
+                          <li>
+                            Python, with knowledge of Python web framework i.e.
+                            Django, Familiarity with ORM (Object Relational
+                            Mapper) libraries.
+                          </li>
                         </ul>
                       </div>
                     </div>
@@ -208,11 +281,19 @@ const AboutUs = () => {
                       </div>
                       <div className="jg-experience-info-list">
                         <ul>
-                          <li>Search Engine Optimization (SEO)</li>
-                          <li>System Testing</li>
-                          <li>Figma · Adobe Photoshop</li>
-                          <li>Responsive Web Design</li>
-                          <li>Problem Solving</li>
+                          <li>
+                            Designed dynamic and interactive pages Using HTML,
+                            CSS, jQuery, and JavaScript.
+                          </li>
+                          <li>Coordinated with global delivery teams.</li>
+                          <li>
+                            Utilised HTML, CSS, and JavaScript to create
+                            Responsive Pages and Forms for Company.
+                          </li>
+                          <li>
+                            Helped Senior in Debugging their code to improve
+                            Website Performance and user experience.
+                          </li>
                         </ul>
                       </div>
                     </div>
